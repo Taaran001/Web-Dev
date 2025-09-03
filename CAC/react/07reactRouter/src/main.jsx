757de32b7cbfacf0e55b3261a75459aeca2483e0
Taaran <taaran06@gmail.com>
2025-09-03 23:31:38 +0530

    Add React Router and Context example projects
    Initial commit for two React projects: one demonstrating React Router setup and another implementing a minimal context API example. Includes configuration files, source code, assets, and documentation for both projects.

---
+++ CAC/react/07reactRouter/src/main.jsx
@@ -0,0 +1,59 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import Layout from "./Layout.jsx"
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, { Loader } from './components/Github/Github.jsx'
+
+
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <Layout/>,
+//         children: [
+//             {
+//                 path: "Header",
+//                 element: <Header />
+//             },
+//             {
+//                 path: "about",
+//                 element: <About />
+//             },
+//             {
+//                 path: "",
+//                 element: <Home />
+//             },
+//             {
+//                 path: "contact",
+//                 element: <Contact />
+//             },
+//             {
+//                 path: "footer",
+//                 element: <Footer />
+//             }
+//         ]
+//     }
+// ])
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="user/:id" element={<User />} />
+            <Route loader={Loader} path="github" element={<Github />} />
+        </Route>
+    )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
