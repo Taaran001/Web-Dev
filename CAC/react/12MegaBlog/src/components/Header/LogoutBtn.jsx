b5f602fdde45abfc8073ab6072ae6c56d6bdf550
Taaran <taaran06@gmail.com>
2025-10-04 03:02:37 +0530

    Add reusable Button and Input components
    Introduced Button and Input components for consistent UI elements. Updated Header to use navigation items based on authentication status and improved LogoutBtn to dispatch logout after service call. Also updated index.js to export new components for easier imports.

--- CAC/react/12MegaBlog/src/components/Header/LogoutBtn.jsx
+++ CAC/react/12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useDispatch } from "react-redux";
-import { authService } from "../../appwrite/config";
+import  authService  from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
 
 function LogoutBtn() {
     const dispatch = useDispatch()
     const logoutHandler = () => {
-        authService.logout()
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
     }
     return (
-
+        <button className='inline-block px6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
     )
 }
 
