33dab6a6b782b290c2476ac84c88ddb80fda0e2a
Taaran <taaran06@gmail.com>
2025-10-06 23:36:16 +0530

    Add blog post CRUD pages and routing
    Introduces pages for adding, editing, viewing, and listing posts. Refactors main.jsx to use React Router and sets up routes for authentication and post management. Adds PostForm component, updates RTE for react-hook-form integration, and fixes imports. Updates store to use authSlice and improves component exports for modularity.

--- CAC/react/12MegaBlog/src/components/index.js
+++ CAC/react/12MegaBlog/src/components/index.js
@@ -3,13 +3,31 @@ import Footer from "./Footer/Footer";
 import  Container  from "./container/Container";
 import  Logo  from "./Logo";
 import  LogoutBtn  from "./Header/LogoutBtn";
-
-
+import  RTE  from "./RTE";
+import  Signup  from "./Signup";
+import  Login  from "./Login";
+import  PostForm  from "./PostForm/PostForm";
+import  PostCard  from "./PostCard";
+// import  Post  from "./Post";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import AuthLayout from "./AuthLayout";
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    // Post,
+    Button,
+    PostCard,
+    PostForm,
+    Input,
+    Select,
+    AuthLayout
 }
