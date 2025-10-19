ebdbb9f16ffd7b8089869f122accfc1b7cca080e
Taaran <taaran06@gmail.com>
2025-10-19 22:44:43 +0530

    Add GitHub user state and fetch logic to App
    Introduced state variables for username, user, repos, and error. Added an async fetchUser function to retrieve GitHub user data and handle 404 errors.

--- Projects/React/githubProfiles/src/App.jsx
+++ Projects/React/githubProfiles/src/App.jsx
@@ -4,11 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [username, setUsername] = useState('')
+    const [user, setUser] = useState(null)
+    const [repos, setRepos] = useState([])
+    const [error, setError] = useState('')
+
+    const fetchUser = async (searchUsername) => {
+        try {
+            const response = await fetch(`https://api.github.com/users/${searchUsername}`)
+
+            if (!response.ok) {
+                if (response.status === 404) {
+                    setError('No profile with this username')
+                    setUser(null)
+                }
+            }
+        } catch (error) {
+
+        }
+    }
 
     return (
         <>
-            
+
         </>
     )
 }
