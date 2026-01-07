aa61ba0c8c79774b48d60306271bf31594760545
Taaran <taaran06@gmail.com>
2026-01-07 05:33:57 +0530

    Add express app setup and utility classes
    Introduced initial express app configuration with CORS and cookie-parser middleware. Added utility classes for standardized API error and response handling, and an async handler for route controllers. Updated dependencies to include cookie-parser and cros. Removed unused .env.sample file.

--- CAC/Backend/src/index.js
+++ CAC/Backend/src/index.js
@@ -7,7 +7,16 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(` Server is running at: ${process.env.PORT}`);
+        
+    })
+})
+.catcj((err)=>{
+    console.log("MONGO DB connection failed !! ", error);
 
+})
 
 
 
