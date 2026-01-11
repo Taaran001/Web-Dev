ec0116236812e52ab68c4ee549e29b7f4f163ecf
Taaran <taaran06@gmail.com>
2026-01-11 22:49:10 +0530

    #13 Add Cloudinary integration, update dependencies, and fix minor issues in middleware and error handling #13

--- CAC/Backend/src/app.js
+++ CAC/Backend/src/app.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import cors from 'cros'
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cros({
+app.use(cors({
     origin: process.env.CROS_ORIGIN,
     credentials: true
 }))
@@ -13,6 +13,16 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter)
+// https://localhost:8000/api/v1/users/register
+
+
 
 
 export { app }
